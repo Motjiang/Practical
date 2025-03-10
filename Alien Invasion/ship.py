e9c82401001d7493ce0d559f0a58d4701ca494e4
Motjiang <motjiangbrilliant@gmail.com>
2025-03-10 19:07:05 +0200

    fixed a typo

--- Alien Invasion/ship.py
+++ Alien Invasion/ship.py
@@ -5,7 +5,7 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load('images/shiip.bmp')
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen.rect.midbottom
